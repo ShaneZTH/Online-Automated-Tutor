0003ff17426f248cb3d3adece9a11c2d4b32a5b6
ShaneZ <shanezhu2@gmail.com>
2020-07-13 00:59:36 -0400

    js file update

--- static/js/academic_main.js
+++ static/js/academic_main.js
@@ -1,12 +1,14 @@
-
 // var pathName = url.pathName.split("\\");
 var pathName = window.location.pathname.split("/");
+var course = pathName[4].toString();
 
 var unreadCount = document.getElementById("unread-posts-count");
 var unreadBtn = document.getElementById("unread-posts-text");
 
 var unansweredCount = document.getElementById("unanswered-posts-count");
 var unansweredBtn = document.getElementById("unanswered-posts-text");
+
+var questionBox = document.getElementById("student-question");
 
 unreadBtn.onclick = function () {
     console.log("unreadBtn is clicked");
@@ -15,11 +17,22 @@ unreadBtn.onclick = function () {
 
 window.onload = function load_numbers() {
     console.log("Loading numbers...");
-    var course = pathName[4].toString();
 
     get_unanswered_count(course);
     get_unread_count(course);
 }
+
+questionBox.addEventListener('keypress', (e) => {
+    // console.log('Got keystroke ' + e.code);
+    if (e.code == 'Enter') {
+        if (e.shiftKey) {
+            e.stopPropagation();
+        } else {
+            e.preventDefault();
+            getAnswer();
+        }
+    }
+});
 
 function get_unanswered_count(course) {
     var settings = {
@@ -46,9 +59,34 @@ function get_unread_count(course) {
             "course": course
         }
     }
+
     // console.log("count_unread() settings: ", settings);
     $.ajax(settings).done(function (response) {
         // console.log("success!", response);
         unreadCount.textContent = response;
+    });
+}
+
+function getAnswer() {
+    var AnswerBox = document.getElementById("student-answer");
+    AnswerBox.textContent = "";
+
+    var userQuestion = $("#student-question").val();
+    console.log("student's question: ", userQuestion)
+
+    var settings = {
+        "url": "/academic/api/v1/answer",
+        "method": "POST",
+        "timeout": 0,
+        "data": {
+            "course": course,
+            "question": userQuestion
+        },
+    };
+
+    // console.log("getAnswer() settings: ", settings);
+    $.ajax(settings).done(function (response) {
+        // console.log("success!", response);
+        AnswerBox.textContent = response;
     });
 }
